4a0836ba8d0f37ff087584ebc0139135b23862a2
Roland Kaminski <kaminski@cs.uni-potsdam.de>
2018-05-11 13:08:35 +0200

    remove unnecessary print statements

--- xorro/tests/transformer_test.py
+++ xorro/tests/transformer_test.py
@@ -29,27 +29,20 @@ def solve(s, mode):
             models[-1].sort()
 
     models.sort()
-    print "models", models
     return models
 
 class TestProgramTransformer(TestCase):
 
     def test_empty_even(self):
-        print "\n\n"
         for mode in ["count", "countp", "up"]:
-            print "test: empty even", mode
             self.assertEqual(solve("&even{ }.", mode), [[]])
 
     def test_empty_odd(self):
-        print "\n\n"
         for mode in ["count", "countp", "up"]:
-            print "test: empty odd", mode
-            self.assertEqual(solve("&odd{ }.", mode), [])    
-        
+            self.assertEqual(solve("&odd{ }.", mode), [])
+
     def test_basic(self):
-        print "\n\n"
         for mode in ["count", "countp", "up"]:
-            print "test: basic", mode
             # run one-elementary parity aggregates
             self.assertEqual(solve("{a;b;c}. &even{ a:a;b:b;c:c }.", mode), [[], ["a", "b"], ['a', 'c'], ['b', 'c']])
             # multiple parity aggregates
@@ -57,12 +50,10 @@ class TestProgramTransformer(TestCase):
             # ...
 
     def test_xor_and_facts(self):
-        print "\n\n"
         for mode in ["count", "countp", "up"]:
-            print "test: xor and facts", mode
             # run one-elementary parity aggregates
             self.assertEqual(solve("{a;b;c}. &even{ a:a;b:b;c:c }. a.", mode), [["a", "b"], ['a', 'c']])
             # multiple parity aggregates
             # complex conditions
             # ...
-        
+
