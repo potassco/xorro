8abd68871a506d4c00dcdd6ff6a1295bdc89bb6c
Roland Kaminski <kaminski@cs.uni-potsdam.de>
2018-05-30 16:29:58 +0200

    refactor countp to use new symbols_to_xor function

--- xorro/countp.py
+++ xorro/countp.py
@@ -8,13 +8,11 @@ the nogood and let clasp to propagate again
 from . import util
 
 class XOR:
-    def __init__(self, literals, parity):
+    def __init__(self, literals):
         self.__literals = literals
-        self.__parity = parity
 
     def check(self, ass):
-        count  = sum(1 for lit in self if ass.is_true(lit))
-        if count % 2 != self.__parity:
+        if not sum(1 for lit in self if ass.is_true(lit)) % 2:
             return [lit if ass.is_true(lit) else -lit for lit in self]
 
     def __iter__(self):
@@ -28,9 +26,21 @@ class CountCheckPropagator:
         for thread_id in range(len(self.__states), init.number_of_threads):
             self.__states.append([])
 
-        constraints, xor_literals = util.symbols_to_xor(init)
-        for constraint in constraints.values():
-            xor = XOR(list(sorted(constraint["literals"])), constraint["parity"])
+        # NOTE: quite a bit of ceremony here
+        #       it would be better to handle these cases here more elegantly
+        #       but this propagator is just a toy anyway
+        def get_lit(atom):
+            lit = init.solver_literal(atom.literal)
+            return lit, init.assignment.value(lit)
+        ret = util.symbols_to_xor_r(init.symbolic_atoms, get_lit)
+        if ret is None:
+            constraints = [[]]
+        else:
+            constraints, facts = ret
+            constraints.extend([fact] for fact in facts)
+
+        for constraint in constraints:
+            xor = XOR(list(sorted(constraint)))
             self.__states[thread_id].append(xor)
 
     def check(self, control):
